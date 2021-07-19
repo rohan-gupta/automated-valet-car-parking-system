3beb0169a7610ee8489c3a5bc50208d826e73740
Rohan Gupta <rohangupta1704@gmail.com>
2021-07-20 02:24:09 +0800

    Added comments to vehicle component

--- app/vehicle/vehiclefactory.py
+++ app/vehicle/vehiclefactory.py
@@ -2,9 +2,12 @@ from .vehicles import car, motorcycle
 
 
 class VehicleFactory:
+    """Factory class to intantiate vehicle objects."""
+
     @staticmethod
     def create_vehicle(vehicle_type, number):
         module_name = vehicle_type.lower()
         class_name = vehicle_type.title()
         args = (vehicle_type, number)
+        # Instantiate the class using args, obtained from module
         return getattr(globals()[module_name], class_name)(*args)

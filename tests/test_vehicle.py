bb03147a79fbfa9b20612d2f6a544c1a39dc6603
Rohan Gupta <rohangupta@Rohans-MacBook-Pro.local>
2021-07-19 13:06:01 +0800

    Updated test

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -1,10 +1,13 @@
-from app.vehicle.vehicles import car, motorcycle
+from app.vehicle.vehicles import vehicle
 from app.vehicle.vehiclefactory import VehicleFactory
 
 
 def test_vehicle():
-    vehicle = VehicleFactory.create_vehicle("car", "SG123")
-    assert isinstance(vehicle, car.Car)
-    assert not isinstance(vehicle, motorcycle.Motorcycle)
-    assert vehicle.vehicle_type == "Car"
-    assert vehicle.number == "SG123"
+    car = VehicleFactory.create_vehicle("car", "SG123")
+    motorcycle = VehicleFactory.create_vehicle("motorcycle", "SG789")
+    assert isinstance(car, vehicle.Vehicle)
+    assert isinstance(motorcycle, vehicle.Vehicle)
+    assert car.vehicle_type == "Car"
+    assert motorcycle.vehicle_type == "Motorcycle"
+    assert car.number == "SG123"
+    assert motorcycle.number == "SG789"
